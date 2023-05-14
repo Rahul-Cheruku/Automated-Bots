f8340aa1e6194bb846744c729bb006e07502031b
Rahul Cheruku <77064752+Rahul-Cheruku@users.noreply.github.com>
2023-05-14 22:21:47 +0530

    Create Instagram-Follow.py

---
+++ Instagram-Follow.py
@@ -0,0 +1,75 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
+# from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import *
+import time
+
+
+
+
+CHROME_DRIVER_PATH = Service("C:/Hunter/development/chromedriver.exe")
+SIMILAR_ACCOUNT = "hebbars.kitchen"
+USERNAME = "vb_tasty_recipes"
+PASSWORD = "password"
+
+options = webdriver.ChromeOptions()
+options.add_argument("user-data-dir=C:/Users/rahul/AppData/Local/Google/Chrome/User Data/Default") #Path to your chrome profile
+
+
+class InstaFollower:
+
+    def __init__(self, path):
+        self.driver = webdriver.Chrome(service=CHROME_DRIVER_PATH, options=options)
+
+    def login(self):
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        time.sleep(5)
+
+        # username = self.driver.find_element_by_name("username")
+        # password = self.driver.find_element_by_name("password")
+        username = self.driver.find_element(by=By.NAME, value="username")
+        password = self.driver.find_element(by=By.NAME, value="password")
+
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
+
+    def find_followers(self):
+        time.sleep(5)
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}")
+
+        time.sleep(2)
+        # followers = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        followers = self.driver.find_element(by=By.XPATH, value='//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        followers.click()
+
+        time.sleep(2)
+        # modal = self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]')
+        modal = self.driver.find_element(by=By.XPATH,value='/html/body/div[4]/div/div/div[2]')
+        for i in range(10):
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
+
+    def follow(self):
+        all_buttons = self.driver.find_elements(by=By.CSS_SELECTOR, value="li button")
+        for button in all_buttons:
+            try:
+                button.click()
+                time.sleep(1)
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_elements(by=By.XPATH, value='/html/body/div[5]/div/div/div/div[3]/button[2]')
+                cancel_button.click()
+
+
+bot = InstaFollower(CHROME_DRIVER_PATH)
+bot.login()
+bot.find_followers()
+bot.follow()
